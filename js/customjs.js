add70604c8cc4627c61151431d11717b05b0eaff
phuhungitc <dophuhung.itc@gmail.com>
2023-05-10 10:53:01 +0700

    update

---
+++ js/customjs.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.header__site__banner__image').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        arrows: false
+    });
+});
